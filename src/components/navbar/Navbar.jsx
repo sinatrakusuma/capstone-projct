f635bf616c596a8394547577a0a37037dd81d32f
ervan19 <ervanmuhammada454@gmail.com>
2022-11-13 22:12:00 +0700

    Edit Navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,64 +1,68 @@
-import React from 'react'
-import "./navbar.scss"
-import Button from '@mui/material/Button'
-import { styled } from '@mui/material/styles'
+import React from "react";
+import "./navbar.scss";
+import Button from "@mui/material/Button";
+import { styled } from "@mui/material/styles";
 // import CustomButton from '../CustomButton/CustomButton'
 
 const KontakButton = styled(Button)({
-  fontSize: '0.75rem',
-  padding: '6px 12px',
-  border: '1px solid',
+  fontSize: "0.75rem",
+  padding: "8px 24px",
   lineHeight: 1.5,
-  backgroundColor: '#2E582C',
-  borderColor: '#2E582C',
-  '&:hover': {
-    backgroundColor: '#2e582ca3',
-    borderColor: '#2e582ca3',
+  backgroundColor: "#2E582C",
+  "&:hover": {
+    backgroundColor: "#2E582C",
+    opacity: 0.8,
   },
-  '&:active': {
-    boxShadow: 'none',
-    backgroundColor: '#2E582C',
-    borderColor: '#2E582C',
+  "&:active": {
+    boxShadow: "none",
   },
-  '&:focus': {
-    boxShadow: '0 0 0 0.1rem rgba(0.0.0.0)',
+  "&:focus": {
+    boxShadow: "0 0 0 0.1rem rgba(0.0.0.0)",
   },
 });
-
-
 
 const Navbar = () => {
   return (
     // <ThemeProvider theme={theme}>
     <header>
-    <div className="wrap">
-      <div className="brand">
-        <h1>DAUSAGI</h1>
-      </div>
-      <nav>
-        <div className="nav-right">
-          <a href="" className="link">Beranda</a>
-          <a href="" className="link">Bank Sampah</a>
-          <a href="" className="link">Daur Ulang</a>
-          <a href="" className="link">Layanan</a>
-          <a href="" className="link">Tentang Kami</a>
-          <KontakButton variant="contained" disableRipple>
-           Kontak
-          </KontakButton>
-          {/* <CustomButton
+      <div className="wrap">
+        <div className="brand">
+          <h1>DAUSAGI</h1>
+        </div>
+        <nav>
+          <div className="nav-right">
+            <a href="" className="link active">
+              Beranda
+            </a>
+            <a href="" className="link">
+              Bank Sampah
+            </a>
+            <a href="" className="link">
+              Daur Ulang
+            </a>
+            <a href="" className="link">
+              Layanan
+            </a>
+            <a href="" className="link">
+              Tentang Kami
+            </a>
+            <KontakButton variant="contained" disableRipple>
+              Kontak
+            </KontakButton>
+            {/* <CustomButton
           backgroundColor='#2E582C'
           color='#ffffff'
           textButton='KONTAK'
           moreBtn={true}
           /> */}
-          {/* <Button variant="contained" sx={{ bgcolor: (theme) => theme.palette.secondary.main,':hover' : bgcolor}} >Kontak</Button> */}
-          {/* <a href="" className="button">Kontak</a> */}
-        </div>
-      </nav>
-    </div>
-   </header>
+            {/* <Button variant="contained" sx={{ bgcolor: (theme) => theme.palette.secondary.main,':hover' : bgcolor}} >Kontak</Button> */}
+            {/* <a href="" className="button">Kontak</a> */}
+          </div>
+        </nav>
+      </div>
+    </header>
     // </ThemeProvider>
-  )
-}
+  );
+};
 
 export default Navbar;
