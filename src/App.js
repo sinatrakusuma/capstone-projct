72f5c90c61fb1e8f8ff85e3052560f7ea809a89c
sinatrakusuma <sinatrakusuma015@gmail.com>
2022-11-13 21:42:34 +0700

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+// import Home from "./pages/home/Home";
+import Kontak from "./pages/kontak/Kontak";
+import React from "react";
+
+function App() {
+
+  return (
+    // <Home/>
+    <Kontak/>
+  );
+}
+
+export default App;
